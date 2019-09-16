da708b0f14ef12b0fbcdd9abeb4cd9bbd2b00ffe
Sushovan <skarmakar@internal.tavisca.com>
2019-09-16 11:28:37 +0530

    Changed the application.properties file

--- src/main/java/com/tavisca/SampleWebApp/HomeController.java
+++ src/main/java/com/tavisca/SampleWebApp/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
     @RequestMapping("home")
     public String home(){
         System.out.println("This is home");
-        return "home.jsp";
+        return "home";
     }
 }
